f8ab0e569b7a9bf361ccab62a30011920280389c
zxyii <148053132+zxyii@users.noreply.github.com>
2023-11-04 20:20:47 +0800

    Add files via upload

---
+++ Homework1.java
@@ -0,0 +1,20 @@
+package Work1;
+
+public class Homework1 {
+    public static void main(String[] args) {
+
+
+    //1.
+        String str="123"; //定义两个字符串型变量
+        String str1="123";
+        String str2=new String("123");  //创建了一个str2对象
+        System.out.println(str==str1);  // 引用的字符串内容相同输出true
+        System.out.println(str==str2); // str2与str内容相同但对象不同所以输出false？
+
+        }
+
+    //2.算法现在会不了一点...
+
+
+
+}
